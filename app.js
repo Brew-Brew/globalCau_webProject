f9319b886df0c3f1ba531dc0161e8b2f22a76035
nuggy875 <jinugpaeng@gmail.com>
2017-12-06 05:04:56 +0900

    design responsive

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
